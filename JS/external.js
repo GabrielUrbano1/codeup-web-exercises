8cfc2bee3bc4542bbad136b656e2f44d7d8abbf5
Gabriel Urbano <gurbano1029@gmail.com>
2023-10-13 10:44:33 -0500

    minor updates to exercises...

--- JS/external.js
+++ JS/external.js
@@ -1,3 +1,7 @@
+
+"use strict";
+
+
 console.log("Hello from External JavaScript")
 
 alert("Welcome to my website!")
@@ -6,17 +10,19 @@ let color = prompt("What is your favorite color")
 //prompts user to input a color
 alert(`Great! ${color} is my favorite color too!`)
 //alerts user that color is also my favorite color...
-let moviescost = prompt("What is the daily cost for the movies?")
+
+//----------------part 2-----------------------
+
+let moviescost = parseInt(prompt("What is the daily cost for the movies?"))
 //this is the base number for the daily rental rate
-let days = prompt("What is the combined days all movies were rented")
+let days = parseInt(prompt("What is the combined days all movies were rented"))
 //adds an on-screen prompt for the user to input the combined number of days all movies have been rented
 let total = (moviescost * days)
 //combines the totals
-alert(`The total amount is $${total}`)
+alert(`The total amount is $${total.toFixed(2)}`)
 //displays for the user to see the total amount the movies will cost.
 
-
-
+//-----------------part 3---------------------
 
 let google = prompt("enter hourly wage for google")
 //prompts user for google base pay
@@ -32,7 +38,5 @@ let fh = prompt("enter how many hours worked")
 //prompts user for hours worked
 let totalpay = (google * gh)+(amazon * ah)+(facebook * fh)
 //does math things for total pay
-alert(`The total pay is $${totalpay}`);
+alert(`The total pay is $${totalpay.toFixed(2)}`);
 //alerts the user what the total pay is
-console.log(`The total pay is $${total}`);
-//also console logs the pay...
