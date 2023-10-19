6c684c5bdade952c5beb0a89d4d659c8a654df7b
Gabriel Urbano <gurbano1029@gmail.com>
2023-10-19 14:05:37 -0500

    completed exercises

---
+++ JS/for_loops.js
@@ -0,0 +1,36 @@
+// //---------this for loop finds if number is even and decrements by 5-------
+// for (let i = 100; i > 0; i -= 5){
+//     if (i % 2 === 0){
+//         console.log(i)
+//     }else {
+//         console.log(i)
+//     }
+// }
+
+
+// this loop generates a random number and returns if the number is odd or even
+for(let i = 0; i < 10; i++){
+    let random = Math.floor(Math.random() * 200) + 20
+    if (random % 2 === 0){
+        console.log(random + " is an even number...")
+    }else {
+        console.log(random + " is an odd number...")
+    }
+}
+
+
+// //-----------shows the multiplication table based on user input from 1-10----------
+// function showMultiplicationTable(num) {
+//     for (let i = 1; i < 11; i++) {
+//         console.log(num + " x " + i + " = "+ num * i)
+//     }
+// }
+//
+// console.log(showMultiplicationTable(2))
+
+
+
+//--------repeats string------
+// for (let i = 0; i < 10; i++){
+//     console.log(i.toString().repeat(i))
+// }
--- Warm-ups/js/warmUps.js
+++ Warm-ups/js/warmUps.js
@@ -7,20 +7,46 @@
 // console.log(`My name is ${name} and I graduated high school in ${year}`);
 
 
-let num = 100
-
-
-while ( num >= 0 ){
-    if (num % 2 == 0){
-        console.log(num + " even")
-    } else{
-        console.log(num)
-    }
-    (num -= 5)
-}
-
-// if (num % 2 = 0){
-//     return num + "even"
-// }else {
-//     return num
+// let num = 100
+//
+//
+// while ( num >= 0 ){
+//     if (num % 2 === 0 && num !== 0){
+//         console.log(num + " is even")
+//     } else{
+//         console.log(num)
+//     }
+//     (num -= 5)
 // }
+
+
+
+// let hello = "hello"
+// for(let i = 0; i < hello.length; i++){
+//     console.log(hello.charAt(i))
+// }
+
+
+//-------------This loop skips the number the user selects....-------------
+// let numberToStopAt = 13
+//     // prompt("Pick an odd number");
+//
+// for (let i = 1; i <= 50; i+=2) {
+//     // console.log("Number to skip is " + numberToStopAt )
+//     if (i == numberToStopAt) {
+//         console.log('Yikes! skipping number ' + numberToStopAt)
+//         break;
+//     }else {
+//         console.log("select an odd number")
+//     }
+//     console.log('here is an odd number: ' + i);
+// }
+//
+
+
+// for (i = 1; i < 50; i +=2){
+//     console.log(i);
+//     continue;
+//
+// }
+
