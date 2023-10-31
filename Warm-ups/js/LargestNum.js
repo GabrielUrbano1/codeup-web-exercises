f89ecb9ae670b2a4e1f674b83299a5b79fc0c533
Gabriel Urbano <gurbano1029@gmail.com>
2023-10-31 09:17:19 -0500

    completed completed box-model exercise.

--- Warm-ups/js/LargestNum.js
+++ Warm-ups/js/LargestNum.js
@@ -1,8 +1,8 @@
+function largestNum(num1, num2, num3){
+    let input1 = num1(prompt("input 1st number"))
+    let input2 = num2(prompt("input 2nd number"))
+    let input3 = num3(prompt("input 3rd number"))
 
-
-
-function largestNum(num1, num2, num3) {
-    console.log(Math.max(num1,num2,num3))
 }
 
-console.log(largestNum(5,10,15))
+console.log(largestNum())
